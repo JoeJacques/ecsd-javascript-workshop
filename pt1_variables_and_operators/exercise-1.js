2441f2782ac920ef14ce580d44f3b4902aee287f
JoeJacques <joe.lewis.jacques@gmail.com>
2020-10-13 23:45:01 +0100

    feat: task1 exercise 1

--- pt1_variables_and_operators/exercise-1.js
+++ pt1_variables_and_operators/exercise-1.js
@@ -6,3 +6,6 @@ let c = {};
 let d = false;
 let e = Infinity;
 let f = undefined;
+
+console.log (`The types of the variables are: ${typeof(a)}, ${typeof(b)}, ${typeof(c)},
+${typeof(d)}, ${typeof(e)} and ${typeof(f)}`)
