bfdcb4918f21e3144c379782026034aaacde7d3e
JoeJacques <joe.lewis.jacques@gmail.com>
2020-10-13 23:35:58 +0100

    feat: task7 exercise2, reading file

--- pt7_callbacks_promises_and_async_actions/exercise-2/read-file.js
+++ pt7_callbacks_promises_and_async_actions/exercise-2/read-file.js
@@ -1,1 +1,13 @@
 // Should read from file which is passed in execution of file
+
+const fs = require('fs')
+file = process.argv[2]
+
+fs.readFile(file, (err, data) => {
+    if (err) {
+        return console.log(`No such file exists ${file}`)
+    }
+        console.log(`Reading file: \n\n ${data.toString()}`)
+})
+
+console.log('test')   // Testing async code. 
