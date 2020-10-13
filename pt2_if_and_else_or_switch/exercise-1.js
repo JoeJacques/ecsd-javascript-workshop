5a2d0559267e1759b01b7e61234945daa2fd0fec
JoeJacques <joe.lewis.jacques@gmail.com>
2020-10-14 00:05:15 +0100

    feat: part2 exercise 1-B

--- pt2_if_and_else_or_switch/exercise-1.js
+++ pt2_if_and_else_or_switch/exercise-1.js
@@ -1,10 +1,15 @@
 // Conditions
 
 let colour = "green";
+colour = 'red'
+colour = 'RED'
 
 
 if (colour === 'GREEN' || colour == 'green') { 
     console.log('color is great'); 
+
+} else if (colour === 'red' || colour === 'RED') {
+    console.log('colour isn\'t nice'); 
 } else {
     console.log('color is unknown')
 }
