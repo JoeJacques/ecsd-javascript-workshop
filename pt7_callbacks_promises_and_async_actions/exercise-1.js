1a6e3b01a5ef85608483e8c5893dacd00dba0afd
JoeJacques <joe.lewis.jacques@gmail.com>
2020-10-13 23:24:35 +0100

    feat: task7 exercise-1

--- pt7_callbacks_promises_and_async_actions/exercise-1.js
+++ pt7_callbacks_promises_and_async_actions/exercise-1.js
@@ -1,15 +1,15 @@
 // When do I run?
 
-console.log("I RUN: 1st");
+console.log("I RUN: 1st"); // Runs First 
 
 const f = () => {
-  console.log("I RUN: 1st");
+  console.log("I RUN: 1st"); // Runs Second 
   setTimeout(() => {
-    console.log("I RUN: 1st");
+    console.log("I RUN: 1st4"); // Runs Last 
   }, 500);
-  console.log("I RUN: 1st");
+  console.log("I RUN: 1st"); // Runs forth
 }
 
 f();
 
-console.log("I RUN: 1st");
+console.log("I RUN: 1st"); // runs fifth. 
