ebc41279b9256c849746f21c718e44c56201828a
JoeJacques <joe.lewis.jacques@gmail.com>
2020-10-14 10:24:36 +0100

    feat: part3-exercise1a, increment array with for loop

--- pt3_functions_and_loops/exercise-1.js
+++ pt3_functions_and_loops/exercise-1.js
@@ -5,29 +5,35 @@
 let array = [1, 5, 8, 9, 15];
 console.log("a) Before function was called: " + array);
 
+const f1 = (array) => {
+    for(let item=0; item < array.length; item++) {
+        ++array[item]
+    }
+}
+
 f1(array);
 console.log("After f1 was called: " + array);
 console.log("\n");
 
-// b) while
+// // b) while
 
-let array2 = [8, 2, 6, 20, 13];
-console.log("b) Before function was called: " + array2);
+// let array2 = [8, 2, 6, 20, 13];
+// console.log("b) Before function was called: " + array2);
 
-// f2(array2);
-// console.log("After f2 was called: " + array2);
-console.log("\n");
+// // f2(array2);
+// // console.log("After f2 was called: " + array2);
+// console.log("\n");
 
-// c) forEach
+// // c) forEach
 
-let array3 = [425, 229, 182, 42, 390];
-console.log("c) The values within array3: " + array3);
+// let array3 = [425, 229, 182, 42, 390];
+// console.log("c) The values within array3: " + array3);
 
-// console.log("The return of f3 on array3: " + f3(array3));
+// // console.log("The return of f3 on array3: " + f3(array3));
 
-// d) map
+// // d) map
 
-let array4 = [800, 600, 1200, 2000, 400];
-console.log("d) The values within array4: " + array4);
+// let array4 = [800, 600, 1200, 2000, 400];
+// console.log("d) The values within array4: " + array4);
 
-// console.log("The return of f4 on a array4: " + f4(array4));
+// // console.log("The return of f4 on a array4: " + f4(array4));
