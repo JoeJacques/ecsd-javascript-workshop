9d03d75d0d3397a8e3b145c99defa46131daf1cb
JoeJacques <joe.lewis.jacques@gmail.com>
2020-10-14 13:50:51 +0100

    feat: task2-a finally-block

--- pt4_try_catch_objects_and_prototypes/exercise-2.js
+++ pt4_try_catch_objects_and_prototypes/exercise-2.js
@@ -1,9 +1,15 @@
 // What's so special about finally?
 
 try {
+    console.log('HELLO')
 
 } catch (error) {
+    console.log(error)
 
 } finally {
+    console.log('I will always run')
 
 }
+
+
+// The significance of the finally block is that it'll always run regardless, 
