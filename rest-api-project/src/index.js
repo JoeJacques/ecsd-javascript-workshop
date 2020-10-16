2565c0da1808028c669af74f1b1dee2feb2ca34b
JoeJacques <joe.lewis.jacques@gmail.com>
2020-10-16 10:33:42 +0100

    feat; testing put

--- rest-api-project/src/index.js
+++ rest-api-project/src/index.js
@@ -1,28 +1,32 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import carRouter from './routes/cars-routes.js';
 
 const PORT = 3000;
 const app = express()
 
+app.use('/cars', carRouter)
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => { 
     res.send( 'hello world');
-});
+})
 
-// app.put('/', async (req, res) =>  { 
-//     res.send(await 'test')
-// })
+app.put('/', (req, res) => { 
+    res.send('PUT TEST')
+})
 
-// app.post('/', async (req, res) => {
-//     res.send(await 'TEST')
-// })
+app.post('/', (req, res) => {
+    res.send('POST ROUTE')
 
-// app.delete('/', async (req, res) => { 
-//     res.send(await 'TEST');
-// })
+})
+
+app.delete('/', (req, res) => { 
+    res.send('DELETE TEST')
+})
+
 
 app.listen(PORT,() =>  {
     console.log(`API is running on port ${PORT}`)
--- rest-api-project/src/routes/cars-routes.js
+++ rest-api-project/src/routes/cars-routes.js
@@ -1,10 +1,27 @@
 import express from 'express'; 
 import bodyParser from 'body-parser'; 
 
+
+
 const router = express.Router(); 
 
-router.get('/cars', (req, res) => { 
-    res.send('Hello this is a test')
+router.get('/', (req, res) => { 
+    res.json({'id': 1, 'make': 'ford', 'model': 'GT 500', 'year': 2001, 'color': 'red'})
+})
+
+router.put('/', (req,res) => { 
+    res.send('test')
+})
+
+router.post('/', (req, res) => {
+    res.send('TESTING')
 
 })
 
+router.delete('/', (req, res) => { 
+    res.send('testing delete')
+
+})
+
+
+export default router; 
