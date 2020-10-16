36eb094e006eba9d7799d1a93a9d3e89ccabeb66
JoeJacques <joe.lewis.jacques@gmail.com>
2020-10-16 14:02:42 +0100

    feat: part-3-exercise-2-a

--- pt3_functions_and_loops/exercise-2.js
+++ pt3_functions_and_loops/exercise-2.js
@@ -5,16 +5,27 @@
 
 // Write your function here...
 
+// This task is confusing for me. I don't understand it. 
+
 buildMap = (num) => { 
-    const square_array = new Array(num)
-    square_array.forEach(Element => square_array.push(num)) 
-    console.log(square_array)
-}
+    const square_array = []
+    for(let i=0; i < num; i++)  {
+        const newRow = []
+        for (let j=0; j < num; j++) {
+            newRow.push([i,j]);
+        }
+        square_array.push(newRow); 
+    }
+    return square_array;
+};
 
 let squareMap = buildMap(5);
 
 console.log(squareMap);
 console.log('\n');
+
+
+// 
 
 // b) Function to build a rectangular Map and return it
 
