391eae9e3e05492b715f173535f335fc5fd5e2d9
JoeJacques <joe.lewis.jacques@gmail.com>
2020-10-16 12:00:05 +0100

    feat: add mongoose database connection localhost:27017/cars

--- rest-api-project/src/index.js
+++ rest-api-project/src/index.js
@@ -1,33 +1,49 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import carRouter from './routes/cars-routes.js';
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+// const carRouter = require('./routes/cars-routes'); 
+const mongoose = require('mongoose')
+const dbConfig = require('../src/data/database.config')
 
 const PORT = 3000;
 const app = express()
+mongoose.Promise = global.Promise;
 
-app.use('/cars', carRouter)
+// app.use('/cars', carRouter)
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
 app.use(cors());
 
+// Connecting to the database
+
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
 app.get('/', (req, res) => { 
-    res.send( 'hello world');
+    res.json( {'id': 1, 'make': 'ford', 'model': 'GT 500', 'year': 2001, 'color': 'red'});
 })
 
-app.put('/', (req, res) => { 
-    res.send('PUT TEST')
-})
+// app.put('/', (req, res) => { 
+//     res.send('PUT TEST')
+// })
 
-app.post('/', (req, res) => {
-    res.send('POST ROUTE')
+// app.post('/', (req, res) => {
+//     res.send('POST ROUTE')
 
-})
+// })
 
-app.delete('/', (req, res) => { 
-    res.send('DELETE TEST')
-})
+// app.delete('/', (req, res) => { 
+//     res.send('DELETE TEST')
+// })
 
 
 app.listen(PORT,() =>  {
     console.log(`API is running on port ${PORT}`)
 })
+
